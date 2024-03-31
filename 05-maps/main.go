2c8f76d9189d4ffe9cdcf87c858b51b08f751f40
shayansaha85 <shayan851997@gmail.com>
2024-03-31 22:36:02 +0530

    dt 31032024 #3

--- 05-maps/main.go
+++ 05-maps/main.go
@@ -2,26 +2,50 @@ package main
 
 import "fmt"
 
+type mappy map[string]string
+
 func main() {
 
 	/*way 1 of creating map*/
 	// var colors map[string]string
 
 	/*way 2 of creating map*/
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
 	/*way 3 of creating map*/
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors["white"] = "#ffffff"
-	colors["red"] = "#ff0000"
+	colors_mappy := mappy{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	fmt.Println(colors)
+	// colors["white"] = "#ffffff"
+	// colors["red"] = "#ff0000"
 
-	delete(colors, "red")
+	// delete(colors, "red")
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+
+	printMap(colors)
+	fmt.Println()
+	colors_mappy.print()
+
+}
+
+func printMap(m map[string]string) {
+	for color, hex := range m {
+		fmt.Println(color, "\t:\t", hex)
+	}
+}
+
+func (m mappy) print() {
+	for color, hex := range m {
+		fmt.Println(color, "\t:\t", hex)
+	}
 }
